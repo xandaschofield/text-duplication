8d4d250c3119379ddad62803142b510778ec546b
Xanda Schofield <xanda@cs.cornell.edu>
2017-01-27 13:12:30 -0500

    Small corrections for better queueing the new exact topic models.

--- bash/train.sh
+++ bash/train.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 fileprefix=$1
-fileidx=$2
-filelist=(../mallet_inputs/$fileprefix*.seq)
+let "fileidx = $2 % 300"
+filelist=(../exact_duplicates_input/$fileprefix*.seq)
 seqfile=${filelist[fileidx]}
 bname=`basename $seqfile .seq`
 for k in 5 10 20 40 80; do
-    outputprefix=../opt_exact_output/$bname-$k
+    outputprefix=../exact_duplicates_output/$bname-$k
     ~/Mallet/bin/mallet train-topics --input $seqfile --num-topics $k --output-state $outputprefix.gz --output-topic-keys $outputprefix.keys --num-top-words 20 --diagnostics-file $outputprefix.diag.xml --evaluator-filename $outputprefix.evaluator --word-topic-counts-file $outputprefix.wordcounts.txt --topic-word-weights-file $outputprefix.wordweights.txt &> $outputprefix.out
 done
